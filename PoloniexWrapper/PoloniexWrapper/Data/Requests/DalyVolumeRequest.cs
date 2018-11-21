f978fd2ce8acd287ade73f773ffb5b66f521e98c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-21 17:37:14 +0200

    Create DalyVolumeRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/DalyVolumeRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class DalyVolumeRequest: BaseRequest
+    {
+        public DalyVolumeRequest():base()
+        {
+            requestArgs = new Dictionary<string, string>
+            {
+                ["command"] = "return24hVolume"
+            };
+        }
+    }
+}
