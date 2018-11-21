fb7b4e064e5d366709a596786035886106f96b65
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-21 14:25:44 +0200

    Тестовая консоль

---
+++ PoloniexWrapper/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using PoloniexWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poloClientPub = new PoloClient();
+            
+            WriteLine("--> " + poloClientPub.GetTickerAsync().Result.FirstOrDefault((ticker => ticker.Key == "USDC_USDT")).Value.LastPrice);
+        }
+    }
+}
