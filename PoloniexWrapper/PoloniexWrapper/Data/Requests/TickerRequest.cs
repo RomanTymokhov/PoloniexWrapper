ab432e7ed23fd65010f1510363b790caff87e56b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-21 12:21:37 +0200

    Create TickerRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/TickerRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class TickerRequest: BaseRequest
+    {
+        public TickerRequest(string apiKey) :base(apiKey)
+        {
+            RequestArgs = new Dictionary<string, string>
+            {
+                ["command"] = "returnTicker"
+            };
+        }
+
+        public override string ToString()
+        {
+            var url = new StringBuilder("?");
+            url.AppendFormat("{0}", BuildRequestData(RequestArgs));
+            return url.ToString();
+        }
+    }
+}
