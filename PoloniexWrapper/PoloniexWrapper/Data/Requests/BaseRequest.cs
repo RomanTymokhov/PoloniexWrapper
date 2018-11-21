45ec9d76045e3aaff9a9757d994889f18aa80f12
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-21 15:08:53 +0200

    Update BaseRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/BaseRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/BaseRequest.cs
@@ -11,8 +11,8 @@ namespace PoloniexWrapper.Data.Requests
         private readonly string apiKey;
 
         internal const string urlSegment = "/public?";
-        
-        internal Dictionary<string, string> RequestArgs { get; set; }
+
+        internal Dictionary<string, string> requestArgs;
 
         public BaseRequest() { }
 
@@ -26,7 +26,8 @@ namespace PoloniexWrapper.Data.Requests
             //todo
         }
 
-        internal static string BuildRequestData(IDictionary<string, string> dict, bool escape = true) => string.Join("&", dict.Select(kvp =>
-                 string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));
+        internal static string BuildRequestData(IDictionary<string, string> dict, bool escape = true) => 
+                        string.Join("&", dict.Select(kvp =>
+                        string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));
     }
 }
