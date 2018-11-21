25e4366aa0d0c0ead0c87266e16efd83391835c6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-21 12:23:12 +0200

    Update PoloClient.cs
    Constructor
    JsonGETAsync<T>
    GetTickerAsync()

--- PoloniexWrapper/PoloniexWrapper/PoloClient.cs
+++ PoloniexWrapper/PoloniexWrapper/PoloClient.cs
@@ -1,8 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PoloniexWrapper.Data.Requests;
+using PoloniexWrapper.Data.Responses;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PoloniexWrapper
 {
-    public class PoloClient
+    public class PoloClient : IDisposable
     {
+        private string ApiKey { get; set; }
+
+        private readonly HttpClient httpClient;
+
+        private const string baseAddress = "https://poloniex.com";
+
+
+        public PoloClient(string apiKey)
+        {
+            ApiKey = apiKey;
+            httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        }
+
+        protected async Task<T> JsonGETAsync<T>(BaseRequest request)
+        {
+            var response = await httpClient.GetAsync(request.ToString()).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();         // throw if web request failed
+            //todo: creae Exception handler
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+        }
+
+
+        public async Task<Ticker> GetTickerAsync() => await JsonGETAsync<Ticker>(new TickerRequest(ApiKey));
+
+
+
+        public void Dispose() => httpClient.Dispose();
     }
 }
