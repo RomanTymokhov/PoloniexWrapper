bdef10b865a33bef75fa2458e2e0163ef55f6dd8
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-22 06:15:38 +0200

    Replace DalyVolume
    Remove DalyVolume --> Data

---
+++ PoloniexWrapper/PoloniexWrapper/Data/DalyVolume.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace PoloniexWrapper.Data
+{
+    public class DalyVolume
+    {
+        public string pairID;
+
+        public string baseName;
+        public decimal? based;
+
+        public string quotedName;
+        public decimal? quoted;
+
+        public DalyVolume(KeyValuePair<string, Dictionary<string, string>> volume)
+        {
+            pairID = volume.Key;
+
+            baseName = volume.Value.First().Key;
+            based = Convert.ToDecimal(volume.Value.First().Value, CultureInfo.InvariantCulture);
+
+            quotedName = volume.Value.Last().Key;
+            quoted = Convert.ToDecimal(volume.Value.Last().Value, CultureInfo.InvariantCulture);
+        }
+    }
+}
