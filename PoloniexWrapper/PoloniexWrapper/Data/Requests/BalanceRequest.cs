c6e33513bc7b7314f12682968d9e4ef24ce49df0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-23 15:43:17 +0200

    Create BalanceRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/BalanceRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class BalanceRequest: BaseRequest
+    {
+        public BalanceRequest():base()
+        {
+            getArgs = new Dictionary<string, string>
+            {
+                ["command"] = "returnBalances"
+            };
+
+            Url = Make(false).Result;
+
+            postArgs = new Dictionary<string, string>
+            {
+                ["tonce"] = GetTonce()
+            };
+
+            POSTdata = 
+        }
+    }
+}
