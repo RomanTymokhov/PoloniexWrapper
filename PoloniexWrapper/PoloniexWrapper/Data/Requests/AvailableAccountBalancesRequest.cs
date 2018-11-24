1454749f790e8ed05cb7c91f82586f7ffd8dca60
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 23:49:38 +0200

    Create AvailableAccountBalancesRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/AvailableAccountBalancesRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+using static PoloniexWrapper.Helper.Enums;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class AvailableAccountBalancesRequest : BaseRequest
+    {
+        public AvailableAccountBalancesRequest(string apiSec, Account account) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnAvailableAccountBalances",
+                ["account"] = account.ToString(),
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
