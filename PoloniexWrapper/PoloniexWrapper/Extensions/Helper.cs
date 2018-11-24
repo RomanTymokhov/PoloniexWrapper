13c3e9229c095fd2656475093ad8727eac09ee1c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 22:23:52 +0200

    Update Helper.cs
    Add ToUnixtime()

--- PoloniexWrapper/PoloniexWrapper/Extensions/Helper.cs
+++ PoloniexWrapper/PoloniexWrapper/Extensions/Helper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using static System.Globalization.CultureInfo;
@@ -7,10 +8,9 @@ namespace PoloniexWrapper.Extensions
 {
     public static class Helper
     {
-        public static string ToKeyValueString(this Dictionary<string, string> dict, bool escape = true)
-        {
-            return string.Join("&", dict.Select(kvp =>
-                   string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));
-        }
+        public static string ToKeyValueString(this Dictionary<string, string> dict, bool escape = true) =>
+                      string.Join("&", dict.Select(kvp => string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));        
+
+        public static string ToUnixtime(this DateTime dateTime) => ((int)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
     }
 }
