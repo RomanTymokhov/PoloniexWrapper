132b862548578b595c0186ea806b0dd2d3141d9a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 11:49:49 +0200

    Create Helper.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Extensions/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static System.Globalization.CultureInfo;
+
+namespace PoloniexWrapper.Extensions
+{
+    public static class Helper
+    {
+        public static string ToKeyValueString(this Dictionary<string, string> dict, bool escape = true)
+        {
+            return string.Join("&", dict.Select(kvp =>
+                   string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));
+        }
+
+        //public static string ToStringHex(this byte[] value)
+        //{
+        //    var output = string.Empty;
+        //    for (var i = 0; i < value.Length; i++)
+        //    {
+        //        output += value[i].ToString("x2", InvariantCulture);
+        //    }
+
+        //    return output;
+        //}
+    }
+}
