3a24c5e933543f5d0b6eca971fd0e58838c20152
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 22:26:47 +0200

    Create DepositsWithdrawals.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/DepositsWithdrawals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class DepositsWithdrawals
+    {
+        [JsonProperty("deposits")]
+        public List<Deposit> DepositList { get; private set; }
+
+        [JsonProperty("withdrawals")]
+        public List<Withdrawal> WithdrawalList { get; private set; }
+    }
+}
