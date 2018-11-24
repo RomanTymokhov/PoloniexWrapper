1c759975fc20f8c2e9eeaace3a66ff8997983f13
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 17:33:13 +0200

    Create CompleteBalance.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/CompleteBalance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class CompleteBalance
+    {
+        [JsonProperty("available")]
+        private readonly string available;
+        public decimal? Availabel => Convert.ToDecimal(available, CultureInfo.InvariantCulture);
+
+        [JsonProperty("onOrders")]
+        private readonly string onOrders;
+        public decimal? OnOrders => Convert.ToDecimal(onOrders, CultureInfo.InvariantCulture);
+
+        [JsonProperty("btcValue")]
+        private readonly string btcValue;
+        public decimal? BtcValue => Convert.ToDecimal(btcValue, CultureInfo.InvariantCulture);
+    }
+}
