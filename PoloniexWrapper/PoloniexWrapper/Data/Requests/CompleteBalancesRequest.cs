36e72f68b456e826b4a481b3676225c0606ad597
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 17:41:24 +0200

    Create CompleteBalancesRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/CompleteBalancesRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class CompleteBalancesRequest : BaseRequest
+    {
+        public CompleteBalancesRequest(string apiSec) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnCompleteBalances",
+                ["nonce"] = GetNonce()
+            }; 
+
+            GenerateRequest(trade);
+        }
+    }
+}
