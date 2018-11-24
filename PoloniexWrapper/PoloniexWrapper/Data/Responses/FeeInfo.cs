bd298d7ff544786be6406ac78a76741dffb04b43
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-25 00:16:40 +0200

    Create FeeInfo.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/FeeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class FeeInfo
+    {
+        [JsonProperty("makerFee")]
+        private readonly string makerFee;
+        public decimal? MakerFee => Convert.ToDecimal(makerFee, CultureInfo.InvariantCulture);
+
+        [JsonProperty("takerFee")]
+        private readonly string takerFee;
+        public decimal? TakerFee => Convert.ToDecimal(takerFee, CultureInfo.InvariantCulture);
+
+        [JsonProperty("thirtyDayVolume")]
+        private readonly string thirtyDayVolume;
+        public decimal? ThirtyDayVolume => Convert.ToDecimal(thirtyDayVolume, CultureInfo.InvariantCulture);
+
+        [JsonProperty("nextTier")]
+        private readonly string nextTier;
+        public decimal? NextTier => Convert.ToDecimal(nextTier, CultureInfo.InvariantCulture);
+    }
+}
