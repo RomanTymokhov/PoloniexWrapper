7c80446d7218309d995256ff11fa5b1ffb9c787d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 18:54:11 +0200

    Create DepositAdressesRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/DepositAdressesRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class DepositAdressesRequest : BaseRequest
+    {
+        public DepositAdressesRequest(string apiSec) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnDepositAddresses",
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
