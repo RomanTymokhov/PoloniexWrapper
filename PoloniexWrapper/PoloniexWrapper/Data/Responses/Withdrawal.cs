9c843db62a94412da17ee63940932280fb3b644b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 22:26:32 +0200

    Create Withdrawal.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/Withdrawal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class Withdrawal
+    {
+        [JsonProperty("withdrawalNumber")]
+        private readonly string witdrawalID;
+        public ulong? WithdrawalID => Convert.ToUInt64(witdrawalID, CultureInfo.InvariantCulture);
+
+        [JsonProperty("currency")]
+        public string CurrencyID { get; private set; }
+
+        [JsonProperty("address")]
+        public string Adress { get; private set; }
+
+        [JsonProperty("amount")]
+        private readonly string amount;
+        public decimal? Amount => Convert.ToDecimal(amount, CultureInfo.InvariantCulture);
+
+        [JsonProperty("timestamp")]
+        private readonly string timestamp;
+        public ulong? Timestamp => Convert.ToUInt64(timestamp, CultureInfo.InvariantCulture);
+
+        [JsonProperty("status")]
+        public string Status { get; private set; }
+
+        [JsonProperty("ipAddress")]
+        public string IpAdress { get; private set; }
+    }
+}
