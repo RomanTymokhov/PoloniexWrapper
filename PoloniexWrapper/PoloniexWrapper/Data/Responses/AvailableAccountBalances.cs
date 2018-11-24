722e52ca6d6f0771882ef36499552b5583a3fc80
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 23:49:48 +0200

    Create AvailableAccountBalances.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/AvailableAccountBalances.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class AvailableAccountBalances
+    {
+        [JsonProperty("exchange")]
+        public Dictionary<string, string> Exchange { get; private set; }
+
+        [JsonProperty("margin")]
+        public Dictionary<string, string> Margin { get; private set; }
+
+        [JsonProperty("lending")]
+        public Dictionary<string, string> Lending { get; private set; }
+    }
+}
