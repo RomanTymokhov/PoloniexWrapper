c5a6d97bbe4e48c0d70ec931922d07d8162a79cd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 22:24:19 +0200

    Create DepositsWithdrawalsRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/DepositsWithdrawalsRequest.cs
@@ -0,0 +1,24 @@
+﻿using PoloniexWrapper.Extensions;
+using System;
+using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class DepositsWithdrawalsRequest :BaseRequest
+    {
+        public DepositsWithdrawalsRequest(string apiSec, DateTime startDate, DateTime endDate) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnDepositsWithdrawals",
+                ["start"] = startDate.ToUnixtime(),
+                ["end"] = endDate.ToUnixtime(),
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
