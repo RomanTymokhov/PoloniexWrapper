271b5cc9e57d417bbaeebff7910ce69457ffeaee
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-24 23:49:18 +0200

    Create Enums.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Helper/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoloniexWrapper.Helper
+{
+    public class Enums
+    {
+        public enum Account { all, exchange, margin, lending }
+    }
+}
