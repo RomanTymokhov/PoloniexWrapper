d8f1def9f3bd32a519aeadf9ac603fccc2b729f3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-25 00:12:06 +0200

    Create FeeInfoRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/FeeInfoRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class FeeInfoRequest : BaseRequest
+    {
+        public FeeInfoRequest(string apiSec) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnFeeInfo",
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
