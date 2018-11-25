2ec70bb1db8a834bd0939c92e7db91d8202105a1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-25 19:46:51 +0200

    Create TradeHistoryRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/TradeHistoryRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using PoloniexWrapper.Helper;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class TradeHistoryRequest : BaseRequest
+    { 
+        public TradeHistoryRequest(string apiSec, string pairID, DateTime? start, DateTime? end, ushort limit) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnTradeHistory",
+                ["currencyPair"] = pairID,
+                ["start"] = start.ToUnixtime(),
+                ["end"] = end.ToUnixtime(),
+                ["limit"] = limit.ToString(),
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
