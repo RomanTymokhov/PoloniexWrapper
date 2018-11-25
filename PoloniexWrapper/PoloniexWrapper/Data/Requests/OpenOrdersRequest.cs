300ef4525fed610bd17e677aec6fd868025bab89
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-25 09:30:16 +0200

    Create OpenOrdersRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OpenOrdersRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Data.Requests.ReqType;
+namespace PoloniexWrapper.Data.Requests
+{
+    public class OpenOrdersRequest : BaseRequest
+    {
+        public OpenOrdersRequest(string apiSec, string pair) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnOpenOrders",
+                ["currencyPair"] = pair,
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(trade);
+        }
+    }
+}
