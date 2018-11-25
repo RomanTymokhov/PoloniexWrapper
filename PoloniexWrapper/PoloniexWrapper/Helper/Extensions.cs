4a77eb398c2d9cc48d1e14efbe57c2207ef6b1b2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-25 20:03:48 +0200

    Update Extensions.cs

--- PoloniexWrapper/PoloniexWrapper/Helper/Extensions.cs
+++ PoloniexWrapper/PoloniexWrapper/Helper/Extensions.cs
@@ -11,10 +11,5 @@ namespace PoloniexWrapper.Helper
                       string.Join("&", dict.Select(kvp => string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));        
 
         public static string ToUnixtime(this DateTime dateTime) => ((int)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
-        public static string ToUnixtime(this DateTime? dateTime)
-        {
-            if (dateTime != null) return ((int)((DateTime)dateTime - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
-            else return null;
-        }
     }
 }
