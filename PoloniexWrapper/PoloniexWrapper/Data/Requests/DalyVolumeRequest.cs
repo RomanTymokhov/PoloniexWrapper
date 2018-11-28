9e7853ce3b4941a2f8e3bcf74143a3f3056ee8f2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-28 20:50:02 +0200

    Update DalyVolumeRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/DalyVolumeRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/DalyVolumeRequest.cs
@@ -4,7 +4,7 @@ using static PoloniexWrapper.Helper.Enums.ReqType;
 
 namespace PoloniexWrapper.Data.Requests
 {
-    public class DalyVolumeRequest: BaseRequest
+    public class DalyVolumeRequest: RequestObjec
     {
         public DalyVolumeRequest():base()
         {
