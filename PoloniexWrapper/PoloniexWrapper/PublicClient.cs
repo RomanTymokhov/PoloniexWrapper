9f3ca181bb9731585fac73c36d568283a2976f0f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-29 07:17:13 +0200

    Create PublicClient.cs

---
+++ PoloniexWrapper/PoloniexWrapper/PublicClient.cs
@@ -0,0 +1,21 @@
+﻿using PoloniexWrapper.Data.Requests;
+using PoloniexWrapper.Data.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoloniexWrapper
+{
+    public class PublicClient : PoloClient
+    {
+        public PublicClient() : base() { }
+
+
+        public async Task<Dictionary<string, Ticker>> ReturnTickerAsync() =>
+               await HttpGetAsync<Dictionary<string, Ticker>>(new TickerRequest());
+
+        public async Task<DalyVolumes> ReturnDalyVolumesAsync() =>
+                await HttpGetAsync<DalyVolumes>(new DalyVolumeRequest());
+    }
+}
