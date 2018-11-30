2dd5508758b483be2899d591aedbd3029e974774
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-30 06:51:33 +0200

    Update DalyVolume.cs

--- PoloniexWrapper/PoloniexWrapper/Data/DalyVolume.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/DalyVolume.cs
@@ -7,6 +7,8 @@ namespace PoloniexWrapper.Data
 {
     public class DalyVolume
     {
+        //Monitor the response and react to the situation if the dictionary "dalyData" has more than 2 KeyValue pairs
+
         public string pairID;
 
         public string baseCurrencyName;
@@ -15,15 +17,19 @@ namespace PoloniexWrapper.Data
         public string quotedCurrencyName;
         public decimal? quotedCurrencyVolume;
 
-        public DalyVolume(Dictionary<string, string> dalyData, string id)
+        public DalyVolume(Dictionary<string, string> dalyc, string pairID)
         {
-            pairID = id;
+            if (dalyc.Count == 2)
+            {
+                this.pairID = pairID;
 
-            baseCurrencyName   = dalyData.First().Key;
-            baseCurrencyVolume = Convert.ToDecimal(dalyData.First().Value, CultureInfo.InvariantCulture);
+                baseCurrencyName = dalyc.First().Key;
+                baseCurrencyVolume = Convert.ToDecimal(dalyc.First().Value, CultureInfo.InvariantCulture);
 
-            quotedCurrencyName   = dalyData.Last().Key;
-            quotedCurrencyVolume = Convert.ToDecimal(dalyData.Last().Value, CultureInfo.InvariantCulture);
+                quotedCurrencyName = dalyc.Last().Key;
+                quotedCurrencyVolume = Convert.ToDecimal(dalyc.Last().Value, CultureInfo.InvariantCulture);
+            }
+            else new Exception("retur 24Volume --> daly volume object not have 2 element");
         }
     }
 }
