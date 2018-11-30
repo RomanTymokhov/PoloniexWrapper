7a4351faac84dc040ecf4fbce79efcb23c9c8edd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-11-30 15:52:46 +0200

    Create OrderTradesRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderTradesRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static PoloniexWrapper.Helper.Enums.ReqType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class OrderTradesRequest : RequestObject
+    {
+        public OrderTradesRequest(string apiSec, ulong? orderNumber) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"]     = "returnOrderTrades",
+                ["orderNumber"] = orderNumber.ToString()
+            };
+
+            GenerateRequest(POST);
+        }
+    }
+}
