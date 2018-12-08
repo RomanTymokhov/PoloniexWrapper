2fccf8c3ecb273e041cb0b816fa2cff361183f91
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-08 11:36:20 +0200

    Create PoloException.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Exceptions/PoloException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Exceptions
+{
+    public class PoloException : Exception
+    {        
+        public PoloException() { }
+
+        public PoloException(string message) : base(message) { }
+
+        public PoloException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
