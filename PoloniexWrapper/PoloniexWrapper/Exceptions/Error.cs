6db5b40bd07c0d996a48cc14a63d2420399b397f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-08 11:36:11 +0200

    Create Error.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Exceptions/Error.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+namespace PoloniexWrapper.Exceptions
+{
+    public class Error
+    {
+        [JsonProperty("error")]
+        public string ErrorMessage { get; private set; }
+    }
+}
