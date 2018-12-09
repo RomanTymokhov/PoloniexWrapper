0b7948710e803031464fedb4ff5384aa41e9c23b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-09 13:59:16 +0200

    Create OrderStatus.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using PoloniexWrapper.Data.Responses.OrderHeirs;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class OrderStatus
+    {
+        [JsonProperty("result")]
+        public object Result { get; private set; }
+
+        [JsonProperty("success")]
+        public byte Success { get; set; }
+    }
+}
