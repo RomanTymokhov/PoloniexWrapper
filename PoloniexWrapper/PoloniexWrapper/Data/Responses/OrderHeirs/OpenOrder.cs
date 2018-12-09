2f15b327c05ed0d26c5a3b3d4fe7fda93cb2adce
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-09 11:27:34 +0200

    Create OpenOrder.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/OpenOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using static PoloniexWrapper.Helper.Enums;
+
+namespace PoloniexWrapper.Data.Responses.OrderHeirs
+{
+    public class OpenOrder
+    {
+        [JsonProperty("orderNumber")]
+        private readonly string orderNumber;
+        public ulong? OrderNumber => Convert.ToUInt64(orderNumber, CultureInfo.InvariantCulture);
+
+        [JsonProperty("type")]
+        public OrderType Type { get; private set; }
+
+        [JsonProperty("rate")]
+        private readonly string rate;
+        public decimal? Rate => Convert.ToDecimal(rate, CultureInfo.InvariantCulture);
+
+        [JsonProperty("amount")]
+        private readonly string amount;
+        public decimal? Amount => Convert.ToDecimal(amount, CultureInfo.InvariantCulture);
+
+        [JsonProperty("total")]
+        private readonly string total;
+        public decimal? Total => Convert.ToDecimal(total, CultureInfo.InvariantCulture);
+
+        [JsonProperty("startingAmount")]
+        private readonly string startingAmount;
+        public decimal? StartingAmount => Convert.ToDecimal(startingAmount, CultureInfo.InvariantCulture);
+
+        [JsonProperty("date")]
+        public DateTime DateTime { get; private set; }
+
+        [JsonProperty("margin")]
+        private readonly string margin;
+        public decimal? Margin => Convert.ToDecimal(margin, CultureInfo.InvariantCulture);
+    }
+}
