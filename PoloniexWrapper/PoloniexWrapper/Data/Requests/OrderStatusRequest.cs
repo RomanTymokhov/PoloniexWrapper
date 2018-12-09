280adc23bd3f13847e91d3a558fc9287bc7102fd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-09 12:59:04 +0200

    Create OrderStatusRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderStatusRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Helper.Enums.RequestType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class OrderStatusRequest : RequestObject
+    {
+        public OrderStatusRequest(string apiSec, ulong? orderNumber) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnOrderStatus",
+                ["orderNumber"] = orderNumber.ToString(),
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(POST);
+        }
+    }
+}
