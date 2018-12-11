fcb53e3caaf08e3415e47b30812172a813f80fb2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-11 18:03:31 +0200

    Create CancelOrderRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/CancelOrderRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using static PoloniexWrapper.Helper.Enums.RequestType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    class CancelOrderRequest : RequestObject
+    {
+        public CancelOrderRequest(string apiSec, ulong orderNumber) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "cancelOrder",
+                ["orderNumber"] = orderNumber.ToString(),
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(POST);
+        }
+    }
+}
