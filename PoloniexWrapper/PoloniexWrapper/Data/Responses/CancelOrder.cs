8c7d266484b44dc6587efe5efe650b6632033112
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-11 18:03:07 +0200

    Create CancelOrder.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/CancelOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class CancelOrder
+    {
+        [JsonProperty("success")]
+        public byte Success { get; set; }
+
+        [JsonProperty("amount")]
+        private readonly string result;
+        public decimal Result => Convert.ToDecimal(result, CultureInfo.InvariantCulture);
+
+        [JsonProperty("message")]
+        public string Message { get; private set; }
+
+        [JsonProperty("error")]
+        public string ErrorMessage { get; private set; }
+    }
+}
