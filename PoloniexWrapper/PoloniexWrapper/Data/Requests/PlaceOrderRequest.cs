a7cbb492ceafbde80db4d13e3cf12a9b9e0fe584
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-11 16:38:47 +0200

    Create PlaceOrderRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/PlaceOrderRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using static PoloniexWrapper.Helper.Enums;
+using static PoloniexWrapper.Helper.Enums.RequestType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class PlaceOrderRequest : RequestObject
+    {
+        public PlaceOrderRequest(string apiSec, OrderType type, decimal rate, decimal amount, string pair) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = type.ToString(),
+                ["rate"] = rate.ToString(),
+                ["amount"] = amount.ToString(),
+                ["currencyPair"] = pair,
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(POST);
+        }
+    }
+}
