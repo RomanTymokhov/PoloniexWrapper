0c67b44e0cb810073f30fb9c4d3175f553916d4e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-14 19:59:50 +0200

    Create MoveOrder.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/MoveOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using PoloniexWrapper.Data.Responses.OrderHeirs;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class MoveOrder
+    {
+        [JsonProperty("success")]
+        public byte Success { get; private set; }
+
+        [JsonProperty("orderNumber")]
+        public ulong? OrderNumber { get; private set; }
+
+        [JsonProperty("resultingTrades")]
+        public Dictionary<string, List<Order>> ResultingTrades { get; set; }
+
+        [JsonProperty("error")]
+        public string ErrorMessage { get; private set; }
+    }
+}
