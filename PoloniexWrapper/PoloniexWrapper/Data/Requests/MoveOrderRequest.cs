98dbf715a6d475dc5b7418dcd18f02466e71a670
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-14 19:59:03 +0200

    Create MoveOrderRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/MoveOrderRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Helper.Enums.RequestType;
+using static System.Globalization.CultureInfo;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class MoveOrderRequest : RequestObject
+    {
+        public MoveOrderRequest(string apiSec, ulong orderNumber, decimal rate, decimal? amount, byte postOnly) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "moveOrder",
+                ["orderNumber"] = orderNumber.ToString(),
+                ["rate"] = rate.ToString(GetCultureInfo("en-US")),
+                ["nonce"] = GetNonce()
+            };
+
+            if (amount != null) arguments.Add("amount", ((decimal)amount).ToString(GetCultureInfo("en-US")));
+            if (postOnly == 1) arguments.Add("postOnly", postOnly.ToString());
+
+            GenerateRequest(POST);
+        }
+    }
+}
