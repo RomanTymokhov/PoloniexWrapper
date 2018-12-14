0aacbf403c8ad6a43ada6ebda138a4f20e59dce1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-14 20:19:22 +0200

    Update DepositsWithdrawals.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/DepositsWithdrawals.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/DepositsWithdrawals.cs
@@ -13,4 +13,31 @@ namespace PoloniexWrapper.Data.Responses
         [JsonProperty("withdrawals")]
         public List<Withdrawal> WithdrawalList { get; private set; }
     }
+
+    public class Withdrawal
+    {
+        [JsonProperty("withdrawalNumber")]
+        private readonly string witdrawalID;
+        public ulong? WithdrawalID => Convert.ToUInt64(witdrawalID, CultureInfo.InvariantCulture);
+
+        [JsonProperty("currency")]
+        public string CurrencyID { get; private set; }
+
+        [JsonProperty("address")]
+        public string Adress { get; private set; }
+
+        [JsonProperty("amount")]
+        private readonly string amount;
+        public decimal? Amount => Convert.ToDecimal(amount, CultureInfo.InvariantCulture);
+
+        [JsonProperty("timestamp")]
+        private readonly string timestamp;
+        public ulong? Timestamp => Convert.ToUInt64(timestamp, CultureInfo.InvariantCulture);
+
+        [JsonProperty("status")]
+        public string Status { get; private set; }
+
+        [JsonProperty("ipAddress")]
+        public string IpAdress { get; private set; }
+    }
 }
