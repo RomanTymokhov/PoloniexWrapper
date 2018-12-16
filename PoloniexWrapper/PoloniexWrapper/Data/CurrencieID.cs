66a21098189ee825f847ae8627ffce42ec0ec805
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 17:51:54 +0200

    Update CurrencieID.cs

--- PoloniexWrapper/PoloniexWrapper/Data/CurrencieID.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/CurrencieID.cs
@@ -10,16 +10,16 @@ namespace PoloniexWrapper.Data
         /// New currency ID will be added here to use.
         /// </summary>
         
-        public const string btc = "BTC";
-        public const string ltc = "LTC";
-        public const string eth = "ETH";
-        public const string etc = "ETC";
-        public const string xrp = "XRP";
-        public const string eos = "EOS";
-        public const string xem = "XEM";
-        public const string sc = "SC";
-        public const string str = "STR";
-        public const string cvc = "CVC";
+        public const string btc  = "BTC";
+        public const string ltc  = "LTC";
+        public const string eth  = "ETH";
+        public const string etc  = "ETC";
+        public const string xrp  = "XRP";
+        public const string eos  = "EOS";
+        public const string xem  = "XEM";
+        public const string sc   = "SC";
+        public const string str  = "STR";
+        public const string cvc  = "CVC";
         public const string usdc = "USDC";
         public const string usdt = "USDT";
     }
