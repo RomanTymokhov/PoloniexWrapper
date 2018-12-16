a95e57d526726e948dada755ffff2b2e512989e8
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 18:25:13 +0200

    Create Currencie.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Currencie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoloniexWrapper.Data
+{
+    public class Currencie
+    {
+        public uint Id { get; private set; }
+
+        public string CurrencieId { get; private set; }
+
+        public CurrencieInfo CurrencieInfo { get; private set; }
+
+        public Currencie(CurrencieInfo currencieInfo, string currencieId)
+        {
+            Id = currencieInfo.Id;
+            CurrencieInfo = currencieInfo;
+            CurrencieId = currencieId;
+        }
+    }
+}
