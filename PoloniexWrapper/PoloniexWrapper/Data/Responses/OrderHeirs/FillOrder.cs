591e35328747ec50c0be9e8d42fcd8cf72ea28bd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 08:40:46 +0200

    Update FillOrder.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/FillOrder.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/FillOrder.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Globalization;
 using Newtonsoft.Json;
 
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
 using static PoloniexWrapper.Helper.Enums;
 
 namespace PoloniexWrapper.Data.Responses.OrderHeirs
@@ -11,13 +12,11 @@ namespace PoloniexWrapper.Data.Responses.OrderHeirs
         [JsonProperty("status")]
         public string Status { get; private set; }
 
-        [JsonProperty("rate")]
-        private readonly string rate;
-        public decimal? Rate => Convert.ToDecimal(rate, CultureInfo.InvariantCulture);
+        private readonly decimal rate;
+        public decimal Rate { get => rate; }
 
-        [JsonProperty("amount")]
-        private readonly string amount;
-        public decimal? Amount => Convert.ToDecimal(amount, CultureInfo.InvariantCulture);
+        private readonly decimal amount;
+        public decimal Amount { get => amount; }
 
         [JsonProperty("currencyPair")]
         public string CurrencyPair { get; private set; }
@@ -25,15 +24,22 @@ namespace PoloniexWrapper.Data.Responses.OrderHeirs
         [JsonProperty("date")]
         public DateTime DateTime { get; private set; }
 
-        [JsonProperty("total")]
-        private readonly string total;
-        public decimal? Total => Convert.ToDecimal(total, CultureInfo.InvariantCulture);
+        private readonly decimal total;
+        public decimal Total { get => total; }
 
         [JsonProperty("type")]
         public OrderType Type { get; private set; }
 
-        [JsonProperty("startingAmount")]
-        private readonly string startingAmount;
-        public decimal? StartingAmount => Convert.ToDecimal(startingAmount, CultureInfo.InvariantCulture);
+        private readonly decimal startingAmount;
+        public decimal StartingAmount { get => startingAmount; }
+
+        [JsonConstructor]
+        public FillOrder(string rate, string amount, string total, string startingAmount)
+        {
+            decimal.TryParse(rate, Any, InvariantCulture, out this.rate);
+            decimal.TryParse(amount, Any, InvariantCulture, out this.amount);
+            decimal.TryParse(total, Any, InvariantCulture, out this.total);
+            decimal.TryParse(startingAmount, Any, InvariantCulture, out this.startingAmount);
+        }
     }
 }
