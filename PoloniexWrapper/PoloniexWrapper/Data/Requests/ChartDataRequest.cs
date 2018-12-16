3e576332983ee6f512589cad69c45546e5d9b31e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 14:59:41 +0200

    Create ChartDataRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/ChartDataRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class ChartDataRequest : RequestObject
+    {
+        public ChartDataRequest(string pairID, uint period, DateTime? start, DateTime? end) : base()
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnChartData",
+                ["currencyPair"] = pairID,
+                ["period"] = period.ToString(),
+                ["nonce"] = GetNonce()
+            };
+
+            if (start != null) arguments.Add("start", start.ToUnixtime());
+            if (end != null) arguments.Add("end", end.ToUnixtime());
+
+            GenerateRequest(GET);
+        }
+    }
+}
