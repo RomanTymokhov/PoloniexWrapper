d6364c8ab4294a421ccf7b7295ba480c7235ef4c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 14:41:42 +0200

    Update TradeHistoryRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/TradeHistoryRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/TradeHistoryRequest.cs
@@ -8,17 +8,33 @@ namespace PoloniexWrapper.Data.Requests
 {
     public class TradeHistoryRequest : RequestObject
     { 
-        public TradeHistoryRequest(string apiSec, DateTime start, DateTime end, string pairID, ushort limit) : base(apiSec)
+        public TradeHistoryRequest(string pairID, DateTime? start, DateTime? end) : base()
         {
             arguments = new Dictionary<string, string>
             {
                 ["command"] = "returnTradeHistory",
                 ["currencyPair"] = pairID,
-                ["start"] = start.ToUnixtime(),
-                ["end"] = end.ToUnixtime(),
-                ["limit"] = limit.ToString(),
                 ["nonce"] = GetNonce()
             };
+
+            if (start != null) arguments.Add("start", start.ToUnixtime());
+            if (end != null) arguments.Add("end", end.ToUnixtime());
+
+            GenerateRequest(GET);
+        }
+
+        public TradeHistoryRequest(string apiSec, string pairID, DateTime? start, DateTime? end, ushort? limit) : base(apiSec)
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnTradeHistory",
+                ["currencyPair"] = pairID,
+                ["nonce"] = GetNonce()
+            };
+
+            if (start != null) arguments.Add("start", start.ToUnixtime());
+            if (end != null) arguments.Add("end", end.ToUnixtime());
+            if (limit != null) arguments.Add("limit", limit.ToString());
 
             GenerateRequest(POST);
         }
