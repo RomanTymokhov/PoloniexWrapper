040f403745f8b246d7e6600d30eda386ecaf927a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 18:32:27 +0200

    Create CurrenciesRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/CurrenciesRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static PoloniexWrapper.Helper.Enums.RequestType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    public class CurrenciesRequest : RequestObject
+    {
+        public CurrenciesRequest() : base()
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnCurrencies",
+                ["nonce"] = GetNonce()
+            };
+
+            GenerateRequest(GET);
+        }
+    }
+}
