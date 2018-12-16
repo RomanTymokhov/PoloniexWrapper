1ee3e115cad1982ea2ef363dbfe0f60ff7218835
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 10:11:06 +0200

    Update DalyVolumes.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/DalyVolumes.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/DalyVolumes.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using Newtonsoft.Json;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
 
 namespace PoloniexWrapper.Data.Responses
 {
@@ -16,97 +18,102 @@ namespace PoloniexWrapper.Data.Responses
         [JsonProperty("BTC_XRP")]
         private Dictionary<string, string> Btc_xrp
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Btc_xrp).ToUpper()));}
+            set => VolumeList.Add(new DalyVolume(value, nameof(Btc_xrp).ToUpper()));
         }
 
         [JsonProperty("BTC_XEM")]
         private Dictionary<string, string> Btc_xem
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Btc_xem).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Btc_xem).ToUpper()));
         }
 
         [JsonProperty("BTC_ETH")]
         private Dictionary<string, string> Btc_eth
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Btc_eth).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Btc_eth).ToUpper()));
         }
 
         [JsonProperty("BTC_SC")]
         private Dictionary<string, string> Btc_sc
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Btc_sc).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Btc_sc).ToUpper()));
         }
 
         [JsonProperty("BTC_EOS")]
         private Dictionary<string, string> Btc_eos
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Btc_eos).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Btc_eos).ToUpper()));
         }
 
         [JsonProperty("ETH_EOS")]
         private Dictionary<string, string> Eth_eos
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Eth_eos).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Eth_eos).ToUpper()));
         }
 
         [JsonProperty("USDC_BTC")]
         private Dictionary<string, string> Usdc_btc
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_btc).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_btc).ToUpper()));
         }
 
         [JsonProperty("USDC_LTC")]
         private Dictionary<string, string> Usdc_ltc
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_ltc).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_ltc).ToUpper()));
         }
 
         [JsonProperty("USDC_STR")]
         private Dictionary<string, string> Usdc_str
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_str).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_str).ToUpper()));
         }
 
         [JsonProperty("USDC_XRP")]
         private Dictionary<string, string> Usdc_xrp
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_xrp).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_xrp).ToUpper()));
         }
 
         [JsonProperty("USDC_ETH")]
         private Dictionary<string, string> Usdc_eth
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_eth).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_eth).ToUpper()));
         }
 
         [JsonProperty("USDC_USDT")]
         private Dictionary<string, string> Usdc_usdt
         {
-            set { VolumeList.Add(new DalyVolume(value, nameof(Usdc_usdt).ToUpper())); }
+            set => VolumeList.Add(new DalyVolume(value, nameof(Usdc_usdt).ToUpper()));
         }
+        
+        private readonly decimal totalBTC;
+        public decimal TotalBTC { get => totalBTC; }
+        
+        private readonly decimal totalETH;
+        public decimal TotalETH { get => totalETH; }
 
-        [JsonProperty("totalBTC")]
-        private readonly string totalBTC;
-        public decimal? TotalBTC => Convert.ToDecimal(totalBTC, CultureInfo.InvariantCulture);
+        private readonly decimal totalUSDC;
+        public decimal TotalUSDC { get => totalUSDC; }
 
-        [JsonProperty("totalETH")]
-        private readonly string totalETH;
-        public decimal? TotalETH => Convert.ToDecimal(totalETH, CultureInfo.InvariantCulture);
+        private readonly decimal totalUSDT;
+        public decimal TotalUSDT { get => totalUSDT; }
 
-        [JsonProperty("totalUSDC")]
-        private readonly string totalUSDC;
-        public decimal? TotalUSDC => Convert.ToDecimal(totalUSDC, CultureInfo.InvariantCulture);
+        private readonly decimal totalXMR;
+        public decimal TotalXMR { get => totalXMR; }
 
-        [JsonProperty("totalUSDT")]
-        private readonly string totalUSDT;
-        public decimal? TotalUSDT => Convert.ToDecimal(totalUSDT, CultureInfo.InvariantCulture);
+        private readonly decimal totalXUSD;
+        public decimal TotalXUSD { get => totalXUSD; }
 
-        [JsonProperty("totalXMR")]
-        private readonly string totalXMR;
-        public decimal? TotalXMR => Convert.ToDecimal(totalXMR, CultureInfo.InvariantCulture);
-
-        [JsonProperty("totalXUSD")]
-        private readonly string totalXUSD;
-        public decimal? TotalXUSD => Convert.ToDecimal(totalXUSD, CultureInfo.InvariantCulture);
+        [JsonConstructor]
+        public DalyVolumes(string totalBTC, string totalETH, string totalUSDC, string totalUSDT, string totalXMR, string totalXUSD)
+        {
+            decimal.TryParse(totalBTC, Any, InvariantCulture, out this.totalBTC);
+            decimal.TryParse(totalETH, Any, InvariantCulture, out this.totalETH);
+            decimal.TryParse(totalUSDC, Any, InvariantCulture, out this.totalUSDC);
+            decimal.TryParse(totalUSDT, Any, InvariantCulture, out this.totalUSDT);
+            decimal.TryParse(totalXMR, Any, InvariantCulture, out this.totalXMR);
+            decimal.TryParse(totalXUSD, Any, InvariantCulture, out this.totalXUSD);
+        }
     }
 }
