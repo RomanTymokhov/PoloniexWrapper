49e44b0032983f70ed843bbb31640aa854010ca2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 15:37:17 +0200

    Update PublicClient.cs
    ReturnChartData --> Complete

--- PoloniexWrapper/PoloniexWrapper/PublicClient.cs
+++ PoloniexWrapper/PoloniexWrapper/PublicClient.cs
@@ -13,9 +13,17 @@ namespace PoloniexWrapper
     {
         public PublicClient() : base() { }
 
+        /// <summary>
+        /// Returns the ticker for all markets
+        /// </summary>
+        /// <returns>Dictionary<string, Ticker></returns>
         public async Task<Dictionary<string, Ticker>> ReturnTickerAsync() =>
                await HttpGetAsync<Dictionary<string, Ticker>>(new TickerRequest());
 
+        /// <summary>
+        /// Returns the 24-hour volume for all markets, plus totals for primary currencies
+        /// </summary>
+        /// <returns>DalyVolumes</returns>
         public async Task<DalyVolumes> ReturnDalyVolumesAsync() =>
                 await HttpGetAsync<DalyVolumes>(new DalyVolumeRequest());
 
@@ -30,13 +38,24 @@ namespace PoloniexWrapper
                 await HttpGetAsync<T>(new OrderBookRequest(pairId, depthSize));
 
         /// <summary>
-        /// Returns the past 200 trades for a given market, or up to 50,000 trades between a range specified in UNIX timestamps by the "start" and "end"
+        /// Returns the past 200 trades for a given market, or up to 50,000 trades between a range specified in DateTime timestamps by the "start" and "end"
         /// </summary>
-        /// <param name="start">UNIX timestamp format</param>
-        /// <param name="end">UNIX timestamp format</param>
+        /// <param name="start">DateTime timestamp format</param>
+        /// <param name="end">DateTime timestamp format</param>
         /// <param name="pairID">currencyPair ID</param>
         /// <returns></returns>
         public async Task<List<PublicTrade>> ReturnTradeHistoryAsync(string pairID, DateTime? start = null, DateTime? end = null) =>
-                await HttpPostAsync<List<PublicTrade>>(new TradeHistoryRequest(pairID, start, end));
+                await HttpGetAsync<List<PublicTrade>>(new TradeHistoryRequest(pairID, start, end));
+
+        /// <summary>
+        /// Returns candlestick chart data
+        /// </summary>
+        /// <param name="pairId">currencyPair</param>
+        /// <param name="period">candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400</param>
+        /// <param name="start">DateTime timestamp format</param>
+        /// <param name="end">DateTime timestamp format</param>
+        /// <returns></returns>
+        public async Task<Chart> ReturnChartDataAsync(string pairId, uint period, DateTime? start = null, DateTime? end = null) =>
+                await HttpGetAsync<Chart>(new ChartDataRequest(pairId, period, start, end));
     }
 }
