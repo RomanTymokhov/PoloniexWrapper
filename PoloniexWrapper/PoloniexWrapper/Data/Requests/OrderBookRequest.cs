787f022c3479a015bc7ed5bb3afefedb6ba10ac2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 08:26:41 +0200

    Create OrderBookRequest.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderBookRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using static PoloniexWrapper.Helper.Enums.RequestType;
+
+namespace PoloniexWrapper.Data.Requests
+{
+    class OrderBookRequest : RequestObject
+    {
+        public OrderBookRequest(string pairId, ushort? depth) : base()
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["command"] = "returnOrderBook",
+                ["currencyPair"] = pairId,
+                ["nonce"] = GetNonce()
+            };
+
+            if (depth != null) arguments.Add("depth", depth.ToString());
+
+            GenerateRequest(GET);
+        }
+    }
+}
