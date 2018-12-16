11d437936661d111d0e94b218586fdb4801b4a69
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 11:54:21 +0200

    Update FeeInfo.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/FeeInfo.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/FeeInfo.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Globalization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
 
 namespace PoloniexWrapper.Data.Responses
 {
     public class FeeInfo
-    {
-        [JsonProperty("makerFee")]
-        private readonly string makerFee;
-        public decimal? MakerFee => Convert.ToDecimal(makerFee, CultureInfo.InvariantCulture);
+    {        
+        private readonly decimal makerFee;
+        public decimal MakerFee { get => makerFee; }
+        
+        private readonly decimal takerFee;
+        public decimal TakerFee { get => takerFee; }
+        
+        private readonly decimal thirtyDayVolume;
+        public decimal ThirtyDayVolume { get => thirtyDayVolume; }
+        
+        private readonly decimal nextTier;
+        public decimal NextTier { get => nextTier; }
 
-        [JsonProperty("takerFee")]
-        private readonly string takerFee;
-        public decimal? TakerFee => Convert.ToDecimal(takerFee, CultureInfo.InvariantCulture);
-
-        [JsonProperty("thirtyDayVolume")]
-        private readonly string thirtyDayVolume;
-        public decimal? ThirtyDayVolume => Convert.ToDecimal(thirtyDayVolume, CultureInfo.InvariantCulture);
-
-        [JsonProperty("nextTier")]
-        private readonly string nextTier;
-        public decimal? NextTier => Convert.ToDecimal(nextTier, CultureInfo.InvariantCulture);
+        [JsonConstructor]
+        public FeeInfo(string makerFee, string takerFee, string thirtyDayVolume, string nextTier)
+        {
+            decimal.TryParse(makerFee, Any, InvariantCulture, out this.makerFee);
+            decimal.TryParse(takerFee, Any, InvariantCulture, out this.takerFee);
+            decimal.TryParse(thirtyDayVolume, Any, InvariantCulture, out this.thirtyDayVolume);
+            decimal.TryParse(nextTier, Any, InvariantCulture, out this.nextTier);
+        }
     }
 }
