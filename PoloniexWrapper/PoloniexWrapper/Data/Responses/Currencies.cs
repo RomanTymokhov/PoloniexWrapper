49b17a61cbcd824e37fd6779bbc5e18465e11473
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-16 18:25:27 +0200

    Create Currencies.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/Currencies.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class Carrencies
+    {
+        /// <summary>
+        /// New currencie will be added here to use
+        /// </summary>
+
+        public List<Currencie> Currencies { get; private set; } = new List<Currencie>();
+
+        [JsonProperty("BTC")]
+        private CurrencieInfo Btc
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Btc).ToUpper()));
+        }
+
+        [JsonProperty("LTC")]
+        private CurrencieInfo Ltc
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Ltc).ToUpper()));
+        }
+
+        [JsonProperty("ETH")]
+        private CurrencieInfo Eth
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Eth).ToUpper()));
+        }
+
+        [JsonProperty("ETC")]
+        private CurrencieInfo Etc
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Etc).ToUpper()));
+        }
+
+        [JsonProperty("XRP")]
+        private CurrencieInfo Xrp
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Xrp).ToUpper()));
+        }
+
+        [JsonProperty("EOS")]
+        private CurrencieInfo Eos
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Eos).ToUpper()));
+        }
+
+        [JsonProperty("XEM")]
+        private CurrencieInfo Xem
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Xem).ToUpper()));
+        }
+
+        [JsonProperty("SC")]
+        private CurrencieInfo Sc
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Sc).ToUpper()));
+        }
+
+        [JsonProperty("STR")]
+        private CurrencieInfo Str
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Str).ToUpper()));
+        }
+
+        [JsonProperty("USDC")]
+        private CurrencieInfo Usdc
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Usdc).ToUpper()));
+        }
+
+        [JsonProperty("USDT")]
+        private CurrencieInfo Usdt
+        {
+            set => Currencies.Add(new Currencie(value, nameof(Usdt).ToUpper()));
+        }
+    }
+}
