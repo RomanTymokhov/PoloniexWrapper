c9514188c2086c3574c0dded50bbf68c13a06b1a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-10 13:50:51 +0200

    Create PoloResponse.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/PoloResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace PoloniexWrapper.Data.Responses
+{
+    public class PoloResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error")]
+        public Error Error { get; set; }
+
+        [JsonProperty("answer")]
+        public object Answer { get; set; }
+    }
+}
