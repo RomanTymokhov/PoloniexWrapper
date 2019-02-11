1ac136f3856ae131ac3eb16bfeab9b1a75c88dab
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-11 08:56:19 +0200

    Update PublicClient.cs

--- PoloniexWrapper/PoloniexWrapper/PublicClient.cs
+++ PoloniexWrapper/PoloniexWrapper/PublicClient.cs
@@ -20,14 +20,12 @@ namespace PoloniexWrapper
         public async Task<ResponseObject> ReturnTickerAsync() =>
             await GenerateAnswer<Dictionary<string, Ticker>>(new TickerRequest());
 
-
         /// <summary>
         /// Returns the 24-hour volume for all markets, plus totals for primary currencies
         /// </summary>
         /// <returns>PoloResponse.Answer -> DalyVolumes</returns>
         public async Task<ResponseObject> ReturnDalyVolumesAsync() =>
             await GenerateAnswer<DalyVolumes>(new DalyVolumeRequest());
-
 
         /// <summary>
         /// Returns the order book for a given market, as well as a sequence number for use with the Push API and an indicator specifying whether the market is frozen
@@ -49,16 +47,16 @@ namespace PoloniexWrapper
         public async Task<ResponseObject> ReturnTradeHistoryAsync(string pairID, DateTime? start = null, DateTime? end = null) =>
                 await GenerateAnswer<List<PublicTrade>>(new TradeHistoryRequest(pairID, start, end));
 
-        ///// <summary>
-        ///// Returns candlestick chart data
-        ///// </summary>
-        ///// <param name="pairId">currencyPair</param>
-        ///// <param name="period">candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400</param>
-        ///// <param name="start">DateTime timestamp format</param>
-        ///// <param name="end">DateTime timestamp format</param>
-        ///// <returns></returns>
-        //public async Task<List<Candlestick>> ReturnChartDataAsync(string pairId, uint period, DateTime start, DateTime end) =>
-        //        await HttpGetAsync<List<Candlestick>>(new ChartDataRequest(pairId, period, start, end));
+        /// <summary>
+        /// Returns candlestick chart data
+        /// </summary>
+        /// <param name="pairId">currencyPair</param>
+        /// <param name="period">candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400</param>
+        /// <param name="start">DateTime timestamp format</param>
+        /// <param name="end">DateTime timestamp format</param>
+        /// <returns>PoloResponse.Answer -> List Candlestick</returns>
+        public async Task<ResponseObject> ReturnChartDataAsync(string pairId, uint period, DateTime start, DateTime end) =>
+                await GenerateAnswer<List<Candlestick>>(new ChartDataRequest(pairId, period, start, end));
 
         ///// <summary>
         ///// Returns information about currencies
