d02f83cd71d9d2833f64cfff19aea0b38ac2c240
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-12 02:02:58 +0200

    Update ResponseObject.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/ResponseObject.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/ResponseObject.cs
@@ -1,16 +1,46 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
 namespace PoloniexWrapper.Data.Responses
 {
     public class ResponseObject
     {
         [JsonProperty("success")]
-        public bool Success { get; set; }
-
-        [JsonProperty("error")]
-        public Error Error { get; set; }
+        public bool Success { get; set; } = true;
 
         [JsonProperty("answer")]
         public object Answer { get; set; }
+
+        private Error error;
+        [JsonProperty("error")]
+        public Error Error
+        {
+            get => error;
+            set
+            {
+                error = value;
+                if (value != null) Success = false;
+            }
+        }
+    }
+    public class Error
+    {
+        [JsonProperty("error")]
+        public string ErrorMessage { get; set; }
+
+        public struct HttpStatus
+        {
+            public ushort code;
+            public string msg;
+        }
+
+        public Error(HttpStatus status, string errMsg = null)
+        {
+            var sb = new StringBuilder("Poloniex API Error: ");
+
+            if (status.code != 200) sb.AppendFormat("{0} - {1}", status.code, status.msg);
+            if (status.code == 200) sb.AppendFormat("{0} - {1}", status.code, errMsg);
+            ErrorMessage = sb.ToString();
+        }
     }
 }
