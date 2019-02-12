83e89d8bfcd9ade12b7c3b9321398caa63d146b7
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-12 02:08:38 +0200

    Update OrderBookRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderBookRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderBookRequest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using static PoloniexWrapper.Helper.Enums.RequestType;
 
 namespace PoloniexWrapper.Data.Requests
