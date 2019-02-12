6dc0e120f8b1987e9f776c741e08e4866ef6f604
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-12 14:08:35 +0200

    Update OrderBook.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderBook.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderBook.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 using static System.Globalization.CultureInfo;
