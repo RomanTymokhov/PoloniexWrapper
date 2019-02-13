1fca6eec04839283ce7d5c5646428c31f3e7754d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-13 13:13:11 +0200

    Update OrderBookRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderBookRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OrderBookRequest.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using static PoloniexWrapper.Helper.Enums.RequestType;
+﻿using static PoloniexWrapper.Helper.Enums.RequestType;
 
 namespace PoloniexWrapper.Data.Requests
 {
@@ -7,14 +6,11 @@ namespace PoloniexWrapper.Data.Requests
     {
         public OrderBookRequest(string pairId, ushort? depth) : base()
         {
-            arguments = new Dictionary<string, string>
-            {
-                ["command"] = "returnOrderBook",
-                ["currencyPair"] = pairId,
-                ["nonce"] = GetNonce()
-            };
+            arguments["command"] = "returnOrderBook";
+            arguments["currencyPair"] = pairId;
+            arguments["nonce"] = GetNonce();
 
-            if (depth != null) arguments.Add("depth", depth.ToString());
+            if (depth != null) arguments["depth"] = depth.ToString();
 
             GenerateRequest(GET);
         }
