f1560296a218f4003aba5db03e214636ecfe25b6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-13 13:11:59 +0200

    Update OpenOrdersRequest.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Requests/OpenOrdersRequest.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Requests/OpenOrdersRequest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-using static PoloniexWrapper.Helper.Enums.RequestType;
+﻿using static PoloniexWrapper.Helper.Enums.RequestType;
 
 namespace PoloniexWrapper.Data.Requests
 {
@@ -8,12 +6,9 @@ namespace PoloniexWrapper.Data.Requests
     {
         public OpenOrdersRequest(string apiSec, string pair) : base(apiSec)
         {
-            arguments = new Dictionary<string, string>
-            {
-                ["command"] = "returnOpenOrders",
-                ["currencyPair"] = pair,
-                ["nonce"] = GetNonce()
-            };
+            arguments["command"] = "returnOpenOrders";
+            arguments["currencyPair"] = pair;
+            arguments["nonce"] = GetNonce();
 
             GenerateRequest(POST);
         }
