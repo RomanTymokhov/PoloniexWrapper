b397507f84d6da9952769d28fb4ec8de02558aae
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-13 12:51:56 +0200

    Update FeeInfo.cs

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/FeeInfo.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/FeeInfo.cs
@@ -8,16 +8,16 @@ namespace PoloniexWrapper.Data.Responses
     public class FeeInfo
     {        
         private readonly decimal makerFee;
-        public decimal MakerFee { get => makerFee; }
+        public decimal MakerFee => makerFee;
         
         private readonly decimal takerFee;
-        public decimal TakerFee { get => takerFee; }
+        public decimal TakerFee => takerFee;
         
         private readonly decimal thirtyDayVolume;
-        public decimal ThirtyDayVolume { get => thirtyDayVolume; }
+        public decimal ThirtyDayVolume => thirtyDayVolume;
         
         private readonly decimal nextTier;
-        public decimal NextTier { get => nextTier; }
+        public decimal NextTier => nextTier;
 
         [JsonConstructor]
         public FeeInfo(string makerFee, string takerFee, string thirtyDayVolume, string nextTier)
