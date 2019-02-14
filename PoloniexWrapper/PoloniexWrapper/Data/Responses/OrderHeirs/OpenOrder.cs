5c68deb60f7b661a1f3d0868ce43cb851e13570e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-14 11:05:58 +0200

    Revert "Update OpenOrder.cs"
    This reverts commit aa0e8964671bcb83c82f2212f3a1a7b85063fb9e.

--- PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/OpenOrder.cs
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/OpenOrder.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Newtonsoft.Json;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+using static PoloniexWrapper.Helper.Enums;
 
 namespace PoloniexWrapper.Data.Responses.OrderHeirs
 {
@@ -8,7 +12,34 @@ namespace PoloniexWrapper.Data.Responses.OrderHeirs
         [JsonProperty("orderNumber")]
         public ulong OrderNumber { get; private set; }
 
-        [JsonProperty("resultingTrades")]
-        public List<Order> ResultingTrades { get; private set; }
+        [JsonProperty("type")]
+        public OrderType Type { get; private set; }
+
+        private readonly decimal rate;
+        public decimal Rate => rate;
+
+        private readonly decimal amount;
+        public decimal Amount => amount;
+
+        private readonly decimal total;
+        public decimal Total => total;
+
+        private readonly decimal startingAmount;
+        public decimal StartingAmount => startingAmount;
+
+        [JsonProperty("date")]
+        public DateTime DateTime { get; private set; }
+
+        [JsonProperty("margin")]
+        public byte Margin { get; private set; }
+
+        [JsonConstructor]
+        public OpenOrder(string rate, string amount, string total, string startingAmount)
+        {
+            decimal.TryParse(rate, Any, InvariantCulture, out this.rate);
+            decimal.TryParse(amount, Any, InvariantCulture, out this.amount);
+            decimal.TryParse(total, Any, InvariantCulture, out this.total);
+            decimal.TryParse(startingAmount, Any, InvariantCulture, out this.startingAmount);
+        }
     }
 }
