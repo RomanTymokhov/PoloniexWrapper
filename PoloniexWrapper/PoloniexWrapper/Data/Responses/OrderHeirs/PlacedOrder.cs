cf78d6633d52864417ab0e44909c608ee6ec1999
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-14 11:15:32 +0200

    Create PlacedOrder.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/PlacedOrder.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PoloniexWrapper.Data.Responses.OrderHeirs
+{
+    public class PlacedOrder
+    {
+        [JsonProperty("orderNumber")]
+        public ulong OrderNumber { get; private set; }
+
+        [JsonProperty("resultingTrades")]
+        public List<Order> ResultingTrades { get; private set; }
+    }
+}
