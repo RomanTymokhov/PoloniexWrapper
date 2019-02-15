a9874038f01121238f40ca031c732d9f97fbb689
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-15 10:20:20 +0200

    Create CanceledOrder.cs

---
+++ PoloniexWrapper/PoloniexWrapper/Data/Responses/OrderHeirs/CanceledOrder.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+
+namespace PoloniexWrapper.Data.Responses.OrderHeirs
+{
+    public class CanceledOrder
+    {
+        [JsonProperty("amount")]
+        private readonly decimal amount;
+        public decimal Amount => amount;
+
+        [JsonProperty("message")]
+        public string Message { get; private set; }
+        
+        public CanceledOrder(string amount, string message)
+        {
+            Message = message;
+            decimal.TryParse(amount, Any, InvariantCulture, out this.amount);
+        }
+    }
+}
